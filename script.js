fbb16cbdc01e354b0627bac8848cfd88e250e270
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-10-14 11:59:02 +0200

    feat: Add theme toggle

---
+++ script.js
@@ -0,0 +1,10 @@
+let darkMode = true;
+
+function toggleTheme() {
+  document.body.classList.toggle("dark-mode");
+
+  let buttonText = document.getElementById("theme-btn").firstChild;
+  buttonText.data = darkMode ? "Dark-Mode" : "Light-Mode";
+
+  darkMode = !darkMode;
+}
